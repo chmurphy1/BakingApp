a3e1c2d4153fc793e72d86f2068bf50eb07f349c
Christopher <christopher.w.murphy1@gmail.com>
2018-10-22 18:14:37 -0600

    Saved data on rotation for steps and instruction detail activity

--- app/src/main/java/murphy/christopher/bakingapp/StepsAndInstructionDetailActivity.java
+++ app/src/main/java/murphy/christopher/bakingapp/StepsAndInstructionDetailActivity.java
@@ -11,6 +11,7 @@ import java.util.List;
 import murphy.christopher.bakingapp.model.DataWrapper;
 import murphy.christopher.bakingapp.model.Ingredients;
 import murphy.christopher.bakingapp.model.Steps;
+import murphy.christopher.bakingapp.utils.Constants;
 
 public class StepsAndInstructionDetailActivity extends AppCompatActivity {
 
@@ -22,15 +23,34 @@ public class StepsAndInstructionDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_steps_and_instruction_detail);
 
-        setupData();
-
+        if(savedInstanceState == null){
+            setupData();
+        }
+        else if(savedInstanceState != null){
+            singleStep = Parcels.unwrap(savedInstanceState.getParcelable(Constants.SINGLE_STEP));
+            ingredientsList = Parcels.unwrap(savedInstanceState.getParcelable(Constants.INGREDIENT_LIST));
+        }
     }
-
     private void setupData(){
         Intent intent = getIntent();
         DataWrapper dw = Parcels.unwrap(intent.getParcelableExtra("data"));
 
         ingredientsList = dw.getIngredientsList();
         singleStep = dw.getSingleStep();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        //Save the single step it's not nul
+        if(singleStep != null){
+            outState.putParcelable(Constants.SINGLE_STEP, Parcels.wrap(singleStep));
+        }
+
+        //Save the ingredient list if it is not null and it's not empty
+        if((ingredientsList != null) && !(ingredientsList.isEmpty())){
+            outState.putParcelable(Constants.INGREDIENT_LIST, Parcels.wrap(ingredientsList));
+        }
     }
 }
--- app/src/main/java/murphy/christopher/bakingapp/utils/Constants.java
+++ app/src/main/java/murphy/christopher/bakingapp/utils/Constants.java
@@ -4,6 +4,8 @@ public class Constants {
     public static final String BASE_URL = "https://d17h27t6h515a5.cloudfront.net/";
     public static final String RECIPE_ADAPTER_KEY = "RECIPE_ADAPTER";
     public static final String RECIPE_KEY = "recipe";
+    public static final String INGREDIENT_LIST = "ingredient_list";
+    public static final String SINGLE_STEP = "single_step";
 
     //needs internationalization later
     public static final String INGREDIENT_STRING = "Ingredients";
